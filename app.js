6e03046b6e1def42fd64873bfe12425928d1f688
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-23 23:46:28 +0530

    first

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-const passportSetup = require("./Backend/api/config/passport-setup");
-const passportSetupAdmin = require("./Backend/api/config/passport-setup-admin");
-const passport = require("passport");
-const cors = require("cors");
-const cookieSession = require("cookie-session");
-const ipfilter = require('express-ipfilter').IpFilter
-const rateLimit = require("express-rate-limit");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const passportSetup = require('./Backend/api/config/passport-setup');
+const passportSetupAdmin = require('./Backend/api/config/passport-setup-admin');
+const passport = require('passport');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const ipfilter = require('express-ipfilter').IpFilter;
+const rateLimit = require('express-rate-limit');
 const schedule = require('node-schedule');
-const sgMail = require("@sendgrid/mail");
-const emailTemplates = require("./Backend/emails/email");
+const sgMail = require('@sendgrid/mail');
+const emailTemplates = require('./Backend/emails/email');
 
 sgMail.setApiKey(process.env.SendgridAPIKey);
 
@@ -19,15 +19,14 @@ sgMail.setApiKey(process.env.SendgridAPIKey);
 
 const app = express();
 
-const userRoutes = require("./Backend/api/routers/user");
-const adminRoutes = require("./Backend/api/routers/admin");
-const quizRoutes = require("./Backend/api/routers/quiz");
-const questionRoutes = require("./Backend/api/routers/questions");
-const authRoutes = require("./Backend/api/routers/auth");
-const authAdminRoutes = require("./Backend/api/routers/auth-admin");
-const generalRoutes = require("./Backend/api/routers/general");
-const ownerRoutes = require("./Backend/api/routers/owner");
-
+const userRoutes = require('./Backend/api/routers/user');
+const adminRoutes = require('./Backend/api/routers/admin');
+const quizRoutes = require('./Backend/api/routers/quiz');
+const questionRoutes = require('./Backend/api/routers/questions');
+const authRoutes = require('./Backend/api/routers/auth');
+const authAdminRoutes = require('./Backend/api/routers/auth-admin');
+const generalRoutes = require('./Backend/api/routers/general');
+const ownerRoutes = require('./Backend/api/routers/owner');
 
 //IMPORT QUIZ AND USERS
 
@@ -35,23 +34,22 @@ const Quiz = require('./Backend/api/models/quiz');
 const User = require('./Backend/api/models/user');
 const Admin = require('./Backend/api/models/admin');
 
-
 const dbURI = process.env.dbURI;
 
 mongoose
-	.connect(dbURI, {
-		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("Database Connected"))
-	.catch((err) => console.log(err));
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database Connected'))
+  .catch((err) => console.log(err));
 
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-console.log(Date.now())
+console.log(Date.now());
 // app.use(cookieSession({
 //     maxAge: 24 * 60 * 60 * 1000,
 //     keys: [keys.cookieSession]
@@ -67,8 +65,8 @@ app.use(passport.session());
 
 /////Rate Limiter
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 150, // limit each IP to 100 requests per windowMs
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 150, // limit each IP to 100 requests per windowMs
 });
 
 //  apply to all requests
@@ -76,92 +74,96 @@ app.use(limiter);
 
 // Allow CORS
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token"
-	);
-	if (req.method === "OPTIONS") {
-		res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-		return res.status(200).json({});
-	}
-	next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization,auth-token'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 
 app.use(cors());
 
-app.use("/user", userRoutes);
-app.use("/admin", adminRoutes);
-app.use("/quiz", quizRoutes);
-app.use("/question", questionRoutes);
-app.use("/auth", authRoutes);
-app.use("/general", generalRoutes);
-app.use("/owner", ownerRoutes);
-app.use("/auth/admin", authAdminRoutes);
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/quiz', quizRoutes);
+app.use('/question', questionRoutes);
+app.use('/auth', authRoutes);
+app.use('/general', generalRoutes);
+app.use('/owner', ownerRoutes);
+app.use('/auth/admin', authAdminRoutes);
 
 app.get('/', async (req, res) => {
-  res.send('hi')
-})
+  res.send('hi');
+});
 
 //route not found
 app.use((req, res, next) => {
-	const error = new Error("Route not found");
-	error.status = 404;
-	next(error);
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-	res.status(error.status || 500);
-	res.json({
-		error: {
-			message: error.message,
-		},
-	});
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
-////SEND REMINDER EMAIL 
+////SEND REMINDER EMAIL
 var rule = new schedule.RecurrenceRule();
 
 rule.minute = new schedule.Range(0, 59, 5);
 
-schedule.scheduleJob(rule, async function(){
+schedule.scheduleJob(rule, async function () {
   let currentTime = Date.now();
   const quizzes = await Quiz.find({
-    reminderSent: {$ne: true},
+    reminderSent: { $ne: true },
     scheduledFor: {
-      $lte: currentTime + 30*60*1000,
-    }
-  })
+      $lte: currentTime + 30 * 60 * 1000,
+    },
+  });
   console.log(`${quizzes.length} number of quizzes need to be reminded`);
 
-  for(let i = 0; i < quizzes.length; i++){
-    console.log(quizzes[i])
-    for(j = 0; j < quizzes[i].usersEnrolled.length; j++){
-      console.log(quizzes[i].usersEnrolled[j])
-      const user = await User.findById(quizzes[i].usersEnrolled[j].userId)
+  for (let i = 0; i < quizzes.length; i++) {
+    console.log(quizzes[i]);
+    for (j = 0; j < quizzes[i].usersEnrolled.length; j++) {
+      console.log(quizzes[i].usersEnrolled[j]);
+      const user = await User.findById(quizzes[i].usersEnrolled[j].userId);
       const msg = {
         to: user.email,
         from: process.env.sendgridEmail,
-        subject: "Quzzie: Quiz Reminder",
-        text: `This is an automatically genrated email sent from Quizzie. This is to remind you that your quiz ${quizzes[i].quizName} is scheduled at ${new Date(Number(quizzes[i].scheduledFor))}, Please login on time to not miss out on your quiz.`,
+        subject: 'Quzzie: Quiz Reminder',
+        text: `This is an automatically genrated email sent from Quizzie. This is to remind you that your quiz ${
+          quizzes[i].quizName
+        } is scheduled at ${new Date(
+          Number(quizzes[i].scheduledFor)
+        )}, Please login on time to not miss out on your quiz.`,
       };
-    
+
       sgMail
         .send(msg)
         .then((result) => {
-          console.log(`Reminder email for ${quizzes[i].quizName} to ${msg.to}`)
+          console.log(`Reminder email for ${quizzes[i].quizName} to ${msg.to}`);
         })
         .catch((err) => {
-          console.log('Some Error Occured', err.toString())
+          console.log('Some Error Occured', err.toString());
         });
-    }  
-    await Quiz.updateOne({ _id: quizzes[i]._id }, { reminderSent: true })
+    }
+    await Quiz.updateOne({ _id: quizzes[i]._id }, { reminderSent: true });
   }
   // console.log(quizzes)
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
